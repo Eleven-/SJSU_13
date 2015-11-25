646f03be324a7a407cec94fc5e73a07a72b19b22
悠二 <michael.chiu@welldoing.net>
2015-11-24 21:00:45 -0800

    api

--- BLE Mission-Center/BLE Mission-Center/CubeSatCommandParser.swift
+++ BLE Mission-Center/BLE Mission-Center/CubeSatCommandParser.swift
@@ -22,24 +22,21 @@ class CubeSatCommandCenter {
     private var readCharacteristic: CBCharacteristic
     private var writeCharacteristic: CBCharacteristic
     private var peripheral: CBPeripheral
-    private let readJPEGCommand: NSData = [UInt8(ReadCurrentJPEGFileContent)].dataValue()
-    private let resetCameraCommand: NSData = [UInt8(RESET)].dataValue()
+    private let readJPEGCommand: NSData     = [UInt8(ReadCurrentJPEGFileContent)].dataValue()
+    private let resetCameraCommand: NSData  = [UInt8(RESET)].dataValue()
     private var lastSendedCommand: UInt8!
     
     private var commandsToSent = [NSData]()
     
     init(readCharacteristic: CBCharacteristic, writeCharacteristic: CBCharacteristic) {
-        print(readCharacteristic)
-        print(writeCharacteristic)
-        self.readCharacteristic = readCharacteristic
-        self.writeCharacteristic = writeCharacteristic
-        self.peripheral = readCharacteristic.service.peripheral
+        self.readCharacteristic     = readCharacteristic
+        self.writeCharacteristic    = writeCharacteristic
+        self.peripheral             = readCharacteristic.service.peripheral
     }
     
     func requestForImage() {
         self.isGettingImg = true
         if !LPCIsBusy {
-            print(readJPEGCommand)
             writeCharacteristic.service.peripheral.writeValue(readJPEGCommand, forCharacteristic: writeCharacteristic, type: .WithResponse)
             lastSendedCommand = UInt8(ReadCurrentJPEGFileContent)
             NSLog("request 4 img, lastSendedCommand %i", lastSendedCommand)
@@ -52,8 +49,8 @@ class CubeSatCommandCenter {
     
     func resetCamera() {
         if !LPCIsBusy {
-            print(resetCameraCommand)
-            writeCharacteristic.service.peripheral.writeValue(resetCameraCommand, forCharacteristic: writeCharacteristic, type: .WithResponse)
+//            writeCharacteristic.service.peripheral.writeValue(resetCameraCommand, forCharacteristic: writeCharacteristic, type: .WithResponse)
+            writeToCubeSat(resetCameraCommand)
             lastSendedCommand = UInt8(RESET)
             NSLog("resetCamera, lastSendedCommand %i", lastSendedCommand)
             LPCIsBusy = true
@@ -91,26 +88,18 @@ class CubeSatCommandCenter {
                     break
                 }
             }
+            writeToCubeSat([lastSendedCommand].dataValue())
         } else {
             LPCIsBusy = false
             requestForImage()
         }
-//        if payload.description.stringByReplacingOccurrencesOfString(" ", withString: "").containsString("ffd9") {
-//            let byteArray = payload.byte_array
-//            var tmp = [UInt8]()
-//            for (index, byte) in byteArray.enumerate() {
-//                tmp.append(byte)
-//                if byte == 0xff && byteArray[index + 1] == 0xd9 {
-//                    tmp.append(byteArray[index + 1])
-//                    break
-//                }
-//                temp.appendData(tmp.dataValue())
-//            }
-//            isGettingImg = false
-//        } else {
-//            temp.appendData(payload)
-//            requestForImage()
-//        }
+    }
+    
+    func writeCommand(bytes: [UInt8]) {
+        self.writeToCubeSat(bytes.dataValue())
+    }
+    private func writeToCubeSat(data: NSData) {
+        writeCharacteristic.service.peripheral.writeValue(data, forCharacteristic: writeCharacteristic, type: .WithResponse)
     }
 }
 
